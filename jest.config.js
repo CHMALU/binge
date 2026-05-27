f211b364e1d376b321c9b3953a313b27ff277904
CHMALU <d.chmalu@gmail.com>
2026-05-27 11:41:17 +0200

    [task][DC,BK] watchlist contract + jest @/ alias
    Shared GET/POST DTOs for US9 + US10 in src/types/watchlist.ts.
    Generic @/* moduleNameMapper so tests can resolve @/auth.

--- jest.config.js
+++ jest.config.js
@@ -6,6 +6,7 @@ module.exports = {
     '^@/components/(.*)$': '<rootDir>/src/components/$1',
     '^@/lib/(.*)$': '<rootDir>/src/lib/$1',
     '^@/app/(.*)$': '<rootDir>/src/app/$1',
+    '^@/(.*)$': '<rootDir>/src/$1',
     '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
   },
   testPathIgnorePatterns: ['<rootDir>/.next/', '<rootDir>/node_modules/'],
